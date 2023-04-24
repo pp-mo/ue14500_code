b52afbeb0334da831cf1da8a241e554d3ebaa723
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2023-04-24 06:03:49 +0100

    Added PNG of logic diagram, and reverse-engineered numpy logic test.

--- logic.py
+++ logic.py
@@ -23,7 +23,7 @@ def seg_a():
 
     print('')
     axb = a ^ b
-    print('AxB')
+    print('AxC')
     pa(axb)
 
     print('')
@@ -33,12 +33,12 @@ def seg_a():
 
     print('')
     half_aeqb = ~axb & (~cxd | c)
-    print('half_aeqb == ~AxB . (~CxD + C)')
+    print('half_aeqb == ~AxC . (~CxD + C)')
     pa(half_aeqb)
 
     print('')
     half_aneb = axb & (cxd | a^c)
-    print('half_aneb == AxB . (CxD + AxC)')
+    print('half_aneb == AxC . (CxD + AxC)')
     pa(half_aneb)
 
     print('')
@@ -108,7 +108,7 @@ def seg_e():
     pa(d)
 
     print('')
-    print('half_d0 = ~D . (~AxB + C)')
+    print('half_d0 = ~D . (~AxC + C)')
     half_d0 = ~d & (~(a ^ b) | c)
     pa(half_d0)
 
@@ -122,4 +122,101 @@ def seg_e():
     full = half_d0 | half_d1
     pa(full)
 
-seg_e()
+
+def disp_7seg(outputs):
+    # Turn an output array into a string 'displaying' 7-segments
+    bit_outputs = [
+        '#' if bit else '_'
+        for bit in outputs
+    ]
+    lines = """
+       00
+     5    1
+     5    1
+       66
+     4    2
+     4    2
+       33
+    """
+    for i_bit in range(7):
+        lines = lines.replace(f'{i_bit}', bit_outputs[i_bit])
+    return lines
+
+
+# seg_e()
+def unpick():
+    a, b, c, d =  ttas(4, (4, 4))
+    A, B, C, D = a, b, c, d
+    
+    AxC = A ^ C
+    BnD = B & D
+    AxD = A ^ D
+
+    X11 = AxC ^ B
+    X12 = BnD ^ C
+    O13 = BnD | AxD
+
+    N21 = ~(X11 | X12)
+    X22 = X11 ^ AxD
+    N23 = ~(X12 | BnD)
+
+    N31 = ~(N21 | AxC)
+    N32 = ~(X22 | AxD)
+    N33 = ~(N23 | A)
+
+    X41 = N32 ^ A
+
+    O51 = N31 | O13
+
+    O61 = N31 | D
+
+    A71 = O61 & X22
+    N72 = ~(N31 | X41)
+    A73 = N31 & D
+    A74 = X41 & B
+    X75 = O51 ^ A
+    N76 = ~(O51 | N23)
+
+    O81 = A73 | A74
+    X82 = A74 ^ N76
+    N83 = ~(X41 | N33)
+
+    o1 = ~N21
+    o2 = ~A71
+    o3 = ~N72
+    o4 = ~O81
+    o5 = ~X75
+    o6 = ~X82
+    o7 = ~N83
+
+    seg_a = o7
+    seg_b = o4
+    seg_c = o6
+    seg_d = o2
+    seg_e = o5
+    seg_f = o3
+    seg_g = o1
+
+    segs = [
+        seg_a,
+        seg_b,
+        seg_c,
+        seg_d,
+        seg_e,
+        seg_f,
+        seg_g,
+    ]
+    for seg in segs:
+        pa(seg.flatten())
+
+    for i in range(16):
+        print('')
+        print(f'N = {i:02x}')
+        outputs = [
+            seg.flatten()[i]
+            for seg in segs
+        ]
+        # outputs = np.array(outputs)
+        print(disp_7seg(outputs))
+
+unpick()
